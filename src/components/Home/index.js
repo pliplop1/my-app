5d35fdedc0d43ca74b0124a7859aeca1cf1ab9bc
pliplop1 <fabien.moise@laposte.net>
2023-01-25 11:58:02 +0100

    Créer la grille de boutons avec « display : grid »

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
+import GridButton from './GridButton';
 
+const list = ['el1', 'el2', 'el3', 'el4'];
 export default function Home() {
     return (
         <Wrapper>
-            <h2>home page</h2>
+            <Grid>
+                {list.map((el) => {
+                    return <GridButton key={el}>{el}</GridButton>;
+                })}
+            </Grid>
         </Wrapper>
     );
 }
+
 const Wrapper = styled.div``;
+
+const Grid = styled.div`
+    display: grid;
+    width: 400px;
+    height: 400px;
+    border: solid 1px;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 12px;
+    row-gap: 12px;
+    padding: 24px;
+    margin: auto;
+
+    @media (max-widht: 640px) {
+        width: 300px;
+        height: 300px;
+    }
+`;
